5312cff4a379adef0aaeac13202b5d19072a783a
Faeyza Rishad <faeyza.rishad@kaist.ac.kr>
2025-06-23 10:54:07 +0900

    temporal correlation

--- dataloader/CIFAR10Dataset.py
+++ dataloader/CIFAR10Dataset.py
@@ -1,6 +1,7 @@
 import os
 
 import torch.utils.data
+from numpy.random import permutation
 from torchvision import transforms
 import numpy as np
 
@@ -9,11 +10,15 @@ class CIFAR10Dataset(torch.utils.data.Dataset):
     std: [0.2471, 0.2435, 0.2616]
     num_classes = 10
 
-    def __init__(self, file_path, domains, transform='src'):
+    def __init__(self, file_path, domains, transform='src', distribution='real', dir_beta=0.1):
         super(CIFAR10Dataset, self).__init__()
+        if distribution not in ['real', 'random', 'dirichlet']:
+            raise NotImplementedError
 
         self.file_path = file_path
         self.domains = domains
+        self.distribution = distribution
+        self.dir_beta = dir_beta
 
         self.features = []
         self.class_labels = []
@@ -31,6 +36,8 @@ class CIFAR10Dataset(torch.utils.data.Dataset):
         self.features = torch.cat(self.features)
         self.class_labels = torch.cat(self.class_labels)
         self.domain_labels = torch.cat(self.domain_labels)
+
+        self.shuffle_distribution()
 
         self.dataset = torch.utils.data.TensorDataset(
             self.features,
@@ -80,3 +87,52 @@ class CIFAR10Dataset(torch.utils.data.Dataset):
         data_path = os.path.join(self.file_path, sub_path1, sub_path2, data_filename)
         label_path = os.path.join(self.file_path, sub_path1, sub_path2, label_filename)
         return data_path, label_path
+
+    def shuffle_distribution(self):
+        # Originally there's a config variable n_samples, skipped here
+        if self.distribution == 'real':
+            return
+
+        rng = np.random.default_rng()
+        permutation = rng.permutation(len(self.class_labels))
+
+        self.features = self.features[permutation]
+        self.class_labels = self.class_labels[permutation]
+        self.domain_labels = self.domain_labels[permutation]
+
+        if self.distribution == 'dirichlet':
+            numchunks = self.num_classes
+            min_size = -1
+            N = len(self.features)
+            min_size_thresh = 10
+            features = []
+            cl_labels = []
+            do_labels = []
+            while min_size < min_size_thresh:
+                idx_batch = [[] for _ in range(numchunks)]
+                idx_batch_cls = [[] for _ in range(numchunks)]
+                for k in range(numchunks):
+                    cl_labels = self.class_labels.numpy()
+                    idx_k = np.where(cl_labels == k)[0]
+
+                    proportions = rng.dirichlet(np.repeat(self.dir_beta, numchunks))
+                    proportions /= proportions.sum()
+                    proportions = (np.cumsum(proportions) * len(idx_k)).astype(int)
+                    idx_batch = [elem + idx.tolist() for elem, idx in zip(idx_batch, np.split(idx_k, proportions))]
+                    min_size = min([len(elem) for elem in idx_batch])
+
+                    for elem, idx in zip(idx_batch_cls, np.split(idx_k, proportions)):
+                        elem.append(idx)
+
+            for chunk in idx_batch_cls:
+                cls_seq = list(range(self.num_classes))
+                rng.shuffle(cls_seq)
+                for cls in cls_seq:
+                    idx = chunk[cls]
+                    features.extend(self.features[idx])
+                    cl_labels.extend(self.domain_labels[idx])
+                    do_labels.extend(self.domain_labels[idx])
+
+            self.features = torch.stack(features)
+            self.domain_labels = torch.LongTensor(do_labels)
+            self.class_labels = torch.LongTensor(cl_labels)
--- model/note.py
+++ model/note.py
@@ -9,11 +9,13 @@ from iabn import convert_iabn
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class NOTE:
-    def __init__(self, source_dataloader, target_dataloader, save_path, iabn=True, alpha=4, momentum=0.1):
+    def __init__(self, source_dataloader, target_dataloader, save_path,
+                 iabn=True, alpha=4, momentum=0.1,):
+        assert (source_dataloader.dataset.num_classes == target_dataloader.dataset.num_classes)
+
         self.device = DEVICE
         self.source_dataloader = source_dataloader
         self.target_dataloader = target_dataloader
-        assert(source_dataloader.dataset.num_classes == target_dataloader.dataset.num_classes)
         self.num_classes = source_dataloader.dataset.num_classes
 
         self.save_path = save_path
@@ -21,12 +23,14 @@ class NOTE:
         self.alpha = alpha
         self.momentum = momentum
 
+        # Init net
         self.net = torchvision.models.resnet18(pretrained=True).to(self.device)
         if self.iabn:
             convert_iabn(self.net, self.alpha)
         num_feats = self.net.fc.in_features
         self.net.fc = nn.Linear(num_feats, self.num_classes)
 
+        # Manage net params
         for param in self.net.parameters():
             param.requires_grad = False
         for module in self.net.modules():
